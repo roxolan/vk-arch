fd9074e11f52b74095cd95fcf717920361a1b17d
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-03 20:00:54 -0400

    oh-my-zsh will be installable only git will be available

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 # initial installation script
 
-# installing oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+timedatectl set-ntp true
+
+# installing oh-my-zsh is postponed until git is present in the system
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
