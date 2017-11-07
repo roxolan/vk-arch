148638deb83176d5b805d18d1b9ccde847d1ee7d
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-06 19:39:23 -0500

    adding initial check for internet connection

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 # initial installation script
 
+wget -q --spider http://archlinux.org
+if [ $? -eq 0 ]; then
+    echo "Internet connection is present. Continuing..."
+else
+    echo "No Internet connection! Exitin..."
+    exit 1
+fi
+
 timedatectl set-ntp true
 
 # installing oh-my-zsh is postponed until git is present in the system
