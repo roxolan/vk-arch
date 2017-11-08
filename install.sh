dd9c99a2052db3ad82e2ef0ab9bcc2520995f889
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-07 19:04:00 -0500

    add partitioning logic

--- install.sh
+++ install.sh
@@ -15,5 +15,10 @@ timedatectl set-ntp true
 
 echo DISK="$DISK", PARTITION="$PARTITION"
 
+parted -s "$DISK" mklabel gpt
+parted -s -a optimal "$DISK" mkpart primary ext4 0% 100%
+parted -s "$DISK" set 1 boot on
+mkfs.ext4 -F "$PARTITION"
+
 # installing oh-my-zsh is postponed until git is present in the system
 # sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
