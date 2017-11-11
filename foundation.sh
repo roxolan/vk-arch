e3987623483dafc563c3ee23cf397b7aacd5aec6
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-10 20:11:22 -0500

    update pacman command on foundation to include other video libraries

--- foundation.sh
+++ foundation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # install video driver
-sudo pacman -S xf86-video-intel
+sudo pacman -Syu xf86-video-intel lib32-mesa-libgl mesa-libgl libva-intel-driver
 
-echo foundation file was run
+echo foundation - installed intel video driver and some libraries
