3d0bf5c881c7c3a58f521bc217dca594cbc763e9
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-11 21:31:47 -0500

    new lines are not accepted in the command with continuators, but comments?

--- foundation.sh
+++ foundation.sh
@@ -4,15 +4,15 @@ sudo pacman -Syu --noconfirm \
      xf86-video-intel \
      lib32-mesa-libgl \
      libva-intel-driver \
-
      xorg-server \
      xorg-server-utils \
-
      zlib \
-
      aspell-en \
-     aspell-uk
+     aspell-uk \
+     zsh \ # trying the comment still
+     python \
+     python2 \
 
 
-echo foundation - installed intel video driver and some libraries with newline symbols and separators
+echo foundation - installed intel video driver and some libraries with line continuators but without newline symbols
 
