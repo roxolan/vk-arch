3b86c6a92ed59a65ea6da05259e804ebe1a26fc0
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-11 21:27:21 -0500

    removing comments in pacman command

--- foundation.sh
+++ foundation.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm \
-     # install video driver
      xf86-video-intel \
      lib32-mesa-libgl \
      libva-intel-driver \
 
-     # install X.Org server
      xorg-server \
      xorg-server-utils \
 
      zlib \
 
-     # spell checkers
      aspell-en \
      aspell-uk
 
