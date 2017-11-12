2cbc5efa634bf8657f8c1993e0018df1efda566e
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-11 23:25:04 -0500

    adding more packages to foundation script

--- foundation.sh
+++ foundation.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm xf86-video-intel libva-intel-driver \
+sudo pacman -Syu --noconfirm reflector \
+     xf86-video-intel libva-intel-driver \
      xorg-server xorg-apps \
-     zlib \
+     acpi lm_sensors \
      aspell-en aspell-uk \
-     zsh \
-     python python2 \
-     htop
+     htop lsof tree \
+     tmux zsh \
+     rsync unzip zlib  \
+     bind-tools net-tools \
+     curl httpie iftop links \
+     the_silver_searcher \
+     terminus-font \
+     xclip xterm \
+     haveged \
+     python python2 
 
-echo foundation - installed intel video driver and some libraries with line continuators grouped a little
+
+echo foundation - installed many packages
 
