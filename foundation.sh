7cac1fb90cb49d3d9fe2fd5f406ba1d2a9b7e907
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-11 21:13:56 -0500

    trying to optimize pacman installation command by batching packages & installing them upfront, configs later

--- foundation.sh
+++ foundation.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# install video driver
-sudo pacman -Syu xf86-video-intel lib32-mesa-libgl mesa-libgl libva-intel-driver
+sudo pacman -Syu --noconfirm \
 
-echo foundation - installed intel video driver and some libraries
+     # install video driver
+     xf86-video-intel \
+     lib32-mesa-libgl \
+     libva-intel-driver \
+
+     # install X.Org server
+     xorg-server \
+     xorg-server-utils
+
+echo foundation - installed intel video driver and some libraries with newline symbols & separators
