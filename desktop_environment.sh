b3d30442165010819b687f66e7750789939a996d
Viktor Kotusenko <roxolanus@gmail.com>
2017-11-23 17:16:56 -0500

    adding some yaourt packages for desktop environment

--- desktop_environment.sh
+++ desktop_environment.sh
@@ -2,4 +2,23 @@
 
 # install i3 & various helpers & fonts & 32-bit GTK libs (for GTK themes support)
 
-yaourt -S i3-wm
+yaourt -S --noconfirm i3-wm i3blocks i3lock-wrapper \
+       polkit \
+       dunst \
+       rofi \
+       xfce4-settings xfce4-screenshooter \
+       compton \
+       feh \
+       xdotool \
+       xkb-switch-git \
+       notify-desktop-git \
+       gnome-keyring \
+       udisks2 udiskie \
+       perl-file-mimeinfo \
+       fontconfig freetype2 cairo \
+       adobe-source-code-pro-fonts ttf-hack ttf-font-awesome ttf-ms-fonts ttf-ubuntu-font-family ttf-symbola ttf-mplus \
+       gtk-engine-murrine \
+       bc
+
+
+
